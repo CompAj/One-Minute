7aa68e5fabc8cfb519c8572d77c614a1479f7fae
CompAj <114016409+CompAj@users.noreply.github.com>
2023-09-15 21:32:17 -0600

    Start of Storm
    Added a circle that encloses over time

--- sketch.js
+++ sketch.js
@@ -2,13 +2,31 @@
 // Anjana Samarasinghe
 // 14-09-2023
 
-let player; 
+let radius;
 
+let player; 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  radius = width;
+  player = new Player();  
 }
 
 function draw() {
-  background(220);
+  background(220); 
+  storm(radius, width, height);
+
+
+  if (radius <= 200){
+    radius = 200
+  }
+  else {
+    radius -=  0.15; 
+  }
 }
+
+function storm(r, width, height) {
+  fill("red");
+  circle(width/2, height/2, r);
+}
+
