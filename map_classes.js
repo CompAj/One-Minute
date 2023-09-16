64584073a951d31be9b390fa89c93601f51dea0a
CompAj <114016409+CompAj@users.noreply.github.com>
2023-09-16 15:16:11 -0600

    Improved on classes
    Player Class:
    +Movement
    +Display
    
    Made the storm into a class:
    +display
    +shrink
    +storm_check

--- map_classes.js
+++ map_classes.js
@@ -10,6 +10,39 @@ class Player {
   }
 
   movement() {
-    this.x ++; 
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+class Storm {
+  constructor(x,y) {
+    this.x = x; 
+    this.y = y; 
+    this.r = x; 
+  }
+
+  display() {
+    fill("red"); 
+    circle(this.x/2, this.y/2, this.r); 
+  }
+
+  shrink() {
+    this.r -= 0.25; 
+  }
+
+  storm_check() {
+    if (storm.r <= 1000){
+      storm.r = 1000;
+    }
+
   }
 }
--- sketch.js
+++ sketch.js
@@ -7,42 +7,26 @@
 
 
 
-let radius;
-
-let powerup_x; 
-
-let powerup_y;
-
-let player = new Player(); 
+let player;
+let storm; 
 
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   radius = width;
-  powerup_x = random(width, height);
-  powerup_y = random(width, height);
-
+  player = new Player(); 
+  storm = new Storm(width,height);
 }
 
 
 function draw() {
   background(220); 
-  // storm(radius, width, height);
   player.movement(); 
-  player.display(); 
-
-
-  if (radius <= 200){
-    radius = 200
-  }
-  else {
-    radius -=  0.15; 
-  }
+  player.display();
+  storm.storm_check(); 
+  storm.shrink(); 
+  //storm.display();
 }
 
-function storm(r, width, height) {
-  fill("red");
-  circle(width/2, height/2, r);
-}
 
