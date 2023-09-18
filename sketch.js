7a93b94b7c979661a04a51d7dcf9ec589f7bd5d1
Qyjj7 <126735053+Qyjj7@users.noreply.github.com>
2023-09-18 14:37:47 -0600

    final version

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 class Player {
-  constructor(x, y, up, down, left, right, shoot, isBot) {
+  constructor(x, y, up, down, left, right, shoot, isBot, sprite) {
     this.bot = isBot;
     this.botTarget = createVector(x, y);
     this.botStart = createVector(x, y);
@@ -7,25 +7,26 @@ class Player {
     this.pos = createVector(x, y);
     this.vel = createVector(0, 0);
     this.direction = createVector(0, 0);
-    this.acceleration = 1;
-    this.topSpeed = 3;
+    this.acceleration = 1*scaling;
+    this.topSpeed = 3*scaling;
     this.weaponAngle = random(0, 2*PI)
     this.weaponPos = createVector(0,0);
     this.weaponBasePos = createVector(0, 0);
-    this.reach = 40
+    this.reach = 40*scaling
     this.throwing = false;
-    this.throwSpeed = 8;
-    this.radius = 20;
+    this.throwSpeed = 8*scaling;
+    this.radius = 20*scaling;
     this.up = up;
     this.down = down;
     this.left = left;
     this.right = right;
     this.shoot = shoot;
     this.immune = true;
-    this.weaponRadius = 12;
+    this.weaponRadius = 12*scaling;
     this.weaponSpin = -1;
     this.spinImmune = false;
     this.dead = false;
+    this.sprite = sprite;
 
     setTimeout(() => {
       this.immune = false;
@@ -33,11 +34,18 @@ class Player {
   }
   
   display() {
-    circle(this.pos.x, this.pos.y, this.radius*2);
+    this.sprite.width = this.radius*2;
+    this.sprite.height = this.radius*2;
+    //circle(this.pos.x, this.pos.y, this.radius*2);
+    image(this.sprite, this.pos.x, this.pos.y);
+
+    push()
+    fill("black")
+    strokeWeight(3)
     line(this.weaponPos.x, this.weaponPos.y, this.weaponBasePos.x, this.weaponBasePos.y);
     circle(this.weaponPos.x, this.weaponPos.y, this.weaponRadius*2);
-    text(this.health, this.pos.x, this.pos.y)
-    //circle(lerp(this.weaponPos.x, this.weaponBasePos.x, 0.5), lerp(this.weaponPos.y, this.weaponBasePos.y, 0.5), this.weaponRadius*2)
+    pop()
+    text(this.health, this.pos.x, this.pos.y + this.radius*2)
   }
   
   tickHealth() {
@@ -57,13 +65,13 @@ class Player {
       }, 100);
     }
 
-    if (this.weaponPos.dist(otherPlayer.pos) < this.weaponRadius + otherPlayer.radius && ! this.immune) {
+    if (this.weaponPos.dist(otherPlayer.pos) < this.weaponRadius + otherPlayer.radius && ! otherPlayer.immune) {
       this.health += 3;
       otherPlayer.health -= 8;
-      this.immune = true;
+      otherPlayer.immune = true;
       setTimeout(() => {
-        this.immune = false;
-      }, 1000);
+        otherPlayer.immune = false;
+      }, 400);
     }
   }
 
@@ -121,7 +129,9 @@ class Player {
 
     //Weapon Mechanics
     if (! this.throwing) {
-      this.weaponAngle += PI / 24 * this.weaponSpin
+      if (! this.immune) {
+        this.weaponAngle += PI / 24 * this.weaponSpin;
+      }
       this.weaponPos.x = this.pos.x + cos(this.weaponAngle)*(this.reach + this.radius);
       this.weaponPos.y = this.pos.y - sin(this.weaponAngle)*(this.reach + this.radius);
     }
@@ -145,23 +155,18 @@ class Player {
   }
 }
 
+function preload() {
+  redImage = loadImage("assets/Yabusame-Red.png")
+  blueImage = loadImage("assets/Yabusame-Blue.png")
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textAlign(CENTER, CENTER);
-  textSize(20)
-  players.push(new Player(0, 0, 87, 83, 65, 68, 32, false)); //w s a d space
-  players.push(new Player(0, height/4, 38, 40, 37, 39, 16, true)) // training dummy upArrow downArrow leftArrow rightArrow Rshift
-  arenaRadius = height/2
-  
-  setInterval(tickAllHealth, 1000)
+  textSize(20);
+  rectMode(CENTER);
+  imageMode(CENTER);  
 
-  console.log("Player 1: WASD move, space to throw")
-}
-
-function draw() {
-  translate(width/2, height/2)
-  background(220);
-  
   if (width > height) {
     arenaRadius = height/2;
   }
@@ -169,15 +174,83 @@ function draw() {
     arenaRadius = width/2;
   }
 
-  circle(0, 0, arenaRadius*2)
+  clockSprites.push(loadImage("assets/clock1.png"));
+  clockSprites.push(loadImage("assets/clock2.png"));
+  clockSprites.push(loadImage("assets/clock3.png"));
+  clockSprites.push(loadImage("assets/clock4.png"));
+  clockSprites.push(loadImage("assets/clock5.png"));
+  clockSprites.push(loadImage("assets/clock6.png"));
+  clockSprites.push(loadImage("assets/clock7.png"));
+  clockSprites.push(loadImage("assets/clock8.png"));
 
-  checkAllCollisions()
+  scaling = arenaRadius / defaultRadius
+  console.log(scaling)
+  
+  setInterval(tickAllHealth, 1000);
+  setInterval(animateClock, 1000);
 
-  for (let somePlayer of players) {
-    somePlayer.checkInput();
-    somePlayer.botWandering();
-    somePlayer.update();
-    somePlayer.display();
+  console.log("Player 1: WASD move, space to throw")
+}
+
+function draw() {
+  translate(width/2, height/2)
+  background(220);
+
+  if (gameState === 0) {
+    text("One Minute Remaining", 0, -100);
+
+    if (keyIsDown(87)) {
+      mode = 0; // 1 player
+    }
+    if (keyIsDown(83)) {
+      mode = 1; // 2 player
+    }
+
+    push();
+    if (mode === 0) {
+      fill(color(171,219,227));
+    }
+    else {
+      fill("white")
+    }
+    rect(0, 0, 100, 30)
+    pop();
+    
+    push();
+    if (mode === 1) {
+      fill(color(171,219,227));
+    }
+    else {
+      fill("white")
+    }
+    rect(0, 50, 100, 30)
+    pop();
+
+    text("1 Player", 0, 0);
+    text("2 Player", 0, 50);
+    text("Press 1 to Start", 0, 100)
+  }
+
+  if (gameState === 1) {
+
+    clockSprites[clockIndex].width = arenaRadius*2;
+    clockSprites[clockIndex].height = arenaRadius*2;
+    image(clockSprites[clockIndex], 0, 0);
+
+    //circle(0, 0, arenaRadius*2)
+    checkAllCollisions()
+    for (let somePlayer of players) {
+      somePlayer.checkInput();
+      somePlayer.botWandering();
+      somePlayer.update();
+      somePlayer.display();
+    }
+
+    if (players.length < 2) {
+      setTimeout(() => {
+        gameState = 0;
+      }, 1500);
+    }
   }
 }
 
@@ -200,5 +273,41 @@ function checkAllCollisions() {
   }
 }
 
+function startGame() {
+  let isBot;
+  if (mode === 0) {
+    isBot = true;
+  }
+  else {
+    isBot = false;
+  }
+  players = [];
+  players.push(new Player(0, -arenaRadius/2, 87, 83, 65, 68, 32, false, redImage)); //w s a d space
+  players.push(new Player(0, arenaRadius/2, 38, 40, 37, 39, 16, isBot, blueImage)) // upArrow downArrow leftArrow rightArrow Rshift
+  gameState = 1;
+}
+
+function animateClock() {
+  clockIndex++
+  if (clockIndex > 7) {
+    clockIndex = 0;
+  }
+}
+
+function keyTyped() {
+  if (key === "1" && gameState === 0) {
+    startGame();
+  }
+}
+
 let players = [];
-let arena;
+let arenaRadius;
+let gameState = 0;
+let mode = 0;
+let redImage;
+let blueImage;
+let clockSprites = [];
+let clockIndex = 0;
+
+let defaultRadius = 371;
+let scale;
